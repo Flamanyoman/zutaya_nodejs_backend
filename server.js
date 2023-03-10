9f3ff2a79514f6c1aee9beb01403f970972b94e4
Flaboy <flamanyoman@gmail.com>
2023-03-10 13:03:14 +0100

    10/03/2023

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.use(
 
 // middleware to allow cookies to be set cross origin
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://zutaya.onrender.com');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET, POST, OPTIONS, PUT, PATCH, DELETE'
